02acf26f3657c076f7951e853108d3d7459aa3de
iskenderColak <iclk84@gmail.com>
2022-11-11 10:25:23 +0300

    Multiple beans of same type, bean name and @Primary Annotation

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/icolak/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/icolak/bean_annotation/ComputerTest.java
@@ -1,10 +1,11 @@
 package com.icolak.bean_annotation;
 
-import com.icolak.bean_annotation.casefactory.Case;
+import com.icolak.bean_annotation.casefactory.DellCase;
 import com.icolak.bean_annotation.config.ComputerConfig;
 import com.icolak.bean_annotation.config.RandomConfig;
 import com.icolak.bean_annotation.monitorfactory.Monitor;
-import com.icolak.bean_annotation.motherboardfactory.Motherboard;
+import com.icolak.bean_annotation.monitorfactory.SonyMonitor;
+import com.icolak.bean_annotation.motherboardfactory.AsusMotherboard;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,19 +14,40 @@ public class ComputerTest {
 
     public static void main(String[] args) {
 
-        // Creating container by using Application Context
+        System.out.println("Creating Container");
+
+        //Creating container by using Application Context
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
 
-        // Creating container by using Bean Factory
+        //Creating container by using BeanFactory
         BeanFactory context = new AnnotationConfigApplicationContext();
 
-        Monitor theMonitor = container.getBean(Monitor.class);
-        Motherboard theMotherboard = container.getBean(Motherboard.class);
-        Case theCase = container.getBean(Case.class);
+        System.out.println("************Retrieving the beans******************");
 
-        PC myPc = new PC(theCase, theMonitor, theMotherboard);
+        SonyMonitor sony = container.getBean("sony2",SonyMonitor.class);
+        DellCase dell = container.getBean(DellCase.class);
+        AsusMotherboard asus = container.getBean(AsusMotherboard.class);
+
+        PC myPc = new PC(dell,sony,asus);
 
         myPc.powerUp();
+
+        dell.pressPowerButton();
+
+        System.out.println("************Retrieving the beans******************");
+
+//        Monitor theMonitor = container.getBean(Monitor.class);
+//        Case theCase = container.getBean(Case.class);
+//        Motherboard theMotherboard = container.getBean(Motherboard.class);
+//
+//        PC myPc2 = new PC(theCase,theMonitor,theMotherboard);
+
+        System.out.println("************Multiple Objects******************");
+
+        Monitor theMonitor2 = container.getBean("monitorSony", Monitor.class);  //DEFAULT BEAN NAME
+        Monitor theMonitor3 = container.getBean("sony2", Monitor.class);  //CUSTOM BEAN NAME
+        Monitor theMonitor4 = container.getBean( Monitor.class);  //@Primary
+
 
     }
 }
