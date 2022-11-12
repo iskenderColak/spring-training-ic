7a8c91e4c69d447102ba1263a9e312b0f2f3a97a
iskenderColak <iclk84@gmail.com>
2022-11-12 10:55:53 +0300

    Using @Qualifier

--- spring-05-Core-Abstraction/src/main/java/com/icolak/proxy/EmailCommentNotificationProxy.java
+++ spring-05-Core-Abstraction/src/main/java/com/icolak/proxy/EmailCommentNotificationProxy.java
@@ -1,9 +1,13 @@
 package com.icolak.proxy;
 
 import com.icolak.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Primary
+@Qualifier("EMAIL")
 public class EmailCommentNotificationProxy implements CommentNotificationProxy {
     @Override
     public void sendComment(Comment comment) {
--- spring-05-Core-Abstraction/src/main/java/com/icolak/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/icolak/service/CommentService.java
@@ -3,6 +3,7 @@ package com.icolak.service;
 import com.icolak.model.Comment;
 import com.icolak.proxy.CommentNotificationProxy;
 import com.icolak.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,9 @@ public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy; // not to forget initializing
 
     // Creating constructor for automatically DI
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,
+                          @Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
+                        // @Qualifier("EMAIL") can be used after naming @Qualifier as EMAIL
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
