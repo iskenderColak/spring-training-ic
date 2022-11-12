326bf58d0ee8dab706db58b07d10040cf8b3b47f
iskenderColak <iclk84@gmail.com>
2022-11-12 10:31:19 +0300

    CommentApp running...

--- spring-05-Core-Abstraction/src/main/java/com/icolak/CommentApp.java
+++ spring-05-Core-Abstraction/src/main/java/com/icolak/CommentApp.java
@@ -1,8 +1,24 @@
 package com.icolak;
 
+import com.icolak.config.CommentConfig;
+import com.icolak.model.Comment;
+import com.icolak.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class CommentApp {
 
     public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService commentService = context.getBean(CommentService.class);
+
+        commentService.publishComment(comment);
 
     }
 }
