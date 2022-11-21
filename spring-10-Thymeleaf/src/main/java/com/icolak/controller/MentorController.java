bc615276002673e7aa6b965ef9cd44401564f492
iskenderColak <iclk84@gmail.com>
2022-11-21 16:39:31 +0300

    Thymeleaf form

--- spring-10-Thymeleaf/src/main/java/com/icolak/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/icolak/controller/MentorController.java
@@ -1,16 +1,35 @@
 package com.icolak.controller;
 
+import com.icolak.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register")
-    public String register() {
+    @GetMapping("/register")
+    public String register(Model model) {
 
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchList);
 
-        return "mentor/register";
+        model.addAttribute("mentor", new Mentor());
+
+        return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+
+//        return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 }
