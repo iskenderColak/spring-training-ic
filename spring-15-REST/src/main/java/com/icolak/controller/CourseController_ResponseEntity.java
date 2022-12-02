579a8d28cba1b289b45b427db724e102fc0b7e81
iskenderColak <iclk84@gmail.com>
2022-12-02 15:37:08 +0300

    Course Controller Response Entity

--- spring-15-REST/src/main/java/com/icolak/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/icolak/controller/CourseController_ResponseEntity.java
@@ -1,4 +1,61 @@
 package com.icolak.controller;
 
+import com.icolak.dto.CourseDTO;
+import com.icolak.entity.Course;
+import com.icolak.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v2")
 public class CourseController_ResponseEntity {
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version", "icolak.V2")
+                .header("Operation", "Get Course List")
+                .body(courseService.getCourses());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId) {
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
+    }
+
+    @GetMapping("/category/{name}")
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category) {
+        return ResponseEntity.ok(courseService.getCoursesByCategory(category));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "icolak.V2")
+                .header("Operation", "Create")
+                .body(courseService.createCourse(courseDTO));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCourseById(@PathVariable("id") long courseId, @RequestBody CourseDTO courseDTO) {
+        courseService.updateCourse(courseId, courseDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") long courseId) {
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
 }
