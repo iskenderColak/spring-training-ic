f129beaf985ad19905f673116894ba26afb50ce2
iskenderColak <iclk84@gmail.com>
2022-12-03 11:12:22 +0300

    Course Controller Response Entity Response Wrapper

---
+++ spring-15-REST/src/main/java/com/icolak/controller/CourseController_ResponseWrapper.java
@@ -0,0 +1,58 @@
+package com.icolak.controller;
+
+import com.icolak.dto.CourseDTO;
+import com.icolak.dto.ResponseWrapper;
+import com.icolak.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses/api/v3")
+public class CourseController_ResponseWrapper {
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseWrapper(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getAllCourses() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version", "icolak.v3")
+                .header("Operation", "Get List")
+                .body(new ResponseWrapper("Course list successfully retrieved", courseService.getCourses()));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Operation", "Get Course")
+                .body(new ResponseWrapper("Course" + courseId + " retrieved successfully", courseService.getCourseById(courseId)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createCourse(@RequestBody CourseDTO courseDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "icolak.V3")
+                .header("Operation", "Create Course")
+                .body(new ResponseWrapper("Course created successfully", courseService.createCourse(courseDTO)));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ResponseWrapper> updateCourseById(@PathVariable("id") long courseId, @RequestBody CourseDTO courseDTO) {
+        courseService.updateCourse(courseId, courseDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<ResponseWrapper> deleteCourseById(@PathVariable("id") long courseId) {
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ spring-15-REST/src/main/java/com/icolak/dto/ResponseWrapper.java
@@ -0,0 +1,30 @@
+package com.icolak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseWrapper {
+
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.message = message;
+        this.data = data;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+}
