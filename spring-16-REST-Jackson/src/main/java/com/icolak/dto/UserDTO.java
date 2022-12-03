5cab84fb9da23612a52352f9a708cff6ce5a5d76
iskenderColak <iclk84@gmail.com>
2022-12-03 12:51:17 +0300

    DTO Package

---
+++ spring-16-REST-Jackson/src/main/java/com/icolak/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.icolak.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.icolak.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String username;
+    private UserRole role;
+    @JsonManagedReference  // This field is going to be serialized
+    private AccountDTO accountDTO;
+}
