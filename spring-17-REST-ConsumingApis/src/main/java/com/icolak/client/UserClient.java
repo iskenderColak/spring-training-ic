a9bbe7e0e6037fceaf2579524c0a24c712663f04
iskenderColak <iclk84@gmail.com>
2022-12-05 12:43:28 +0300

    Feign Client Consume

---
+++ spring-17-REST-ConsumingApis/src/main/java/com/icolak/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.icolak.client;
+
+import com.icolak.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com", name = "USER-CLIENT")
+public interface UserClient {
+
+    @GetMapping("/users")
+    List<User> getUsers();
+
+}
