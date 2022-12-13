c60618cb9b8494a66a49da07d38016ab2b4e8a89
iskenderColak <iclk84@gmail.com>
2022-12-13 11:45:59 +0300

    Course Repository and sample queries

---
+++ spring-14-Data-Queries/src/main/java/com/icolak/repository/CourseRepository.java
@@ -0,0 +1,31 @@
+package com.icolak.repository;
+
+import com.icolak.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    //Find all courses by category
+    List<Course>  findByCategory(String category);
+
+    //Find all courses by category and order the entities by name
+    List<Course> findAllByCategoryOrOrderByName(String category);
+
+    //Checks if a course with the supplied name exists. Return true if exists, false otherwise
+    Boolean existsByName(String name);
+
+    //Returns the count of courses for the given category
+    Integer countAllByCategory(String country);
+
+    //Finds all the courses that start with the given course name string
+    List<Course> findAllByNameStartsWith(String name);
+
+    //Find all courses by category and returns a stream
+    Stream<Course> streamAllByCategory(String category);
+}
+
