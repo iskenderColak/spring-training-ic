3cc140d47caaaa3ed58477d4686cee6033e2b36b
iskenderColak <iclk84@gmail.com>
2023-01-11 13:20:05 +0300

    UnitTesting parameterized tests @ValueSource

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorParameterizedTest {
@@ -12,7 +15,16 @@ public class CalculatorParameterizedTest {
 
     @ParameterizedTest
     @ValueSource(ints = {2, 6, 9})
-    void testCase1(int num) {
+    void testCase2(int num) {
         Assertions.assertEquals(0, num % 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void testCase3(String arg) {
+        Assertions.assertFalse(arg.isEmpty());
     }
 }
