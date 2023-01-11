95c64c7af5a997c0afbbc096dd15c18674c7a3bf
iskenderColak <iclk84@gmail.com>
2023-01-11 12:57:03 +0300

    Unit Testing some methods

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,14 +1,46 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.AccessDeniedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @BeforeAll
+    static void setUpAll() { // !!! it is static method @BeforeAll
+        System.out.println("BeforeAll is executed");
+    }
+            // These two methods are common, so we create them as static
+    @AfterAll
+    static void tearDownAll() { // !!! it is static method @AfterAll
+        System.out.println("AfterAll is executed");
+    }
+
+    @BeforeEach
+    void setUpEach() {
+        System.out.println("BeforeEach is executed");
+    }
+
+    @AfterEach
+    void tearDownEach() {
+        System.out.println("AfterEach is executed");
+    }
 
     @Test
     void add() {
         System.out.println("Add method");
         int actual = Calculator.add(2,3);
         assertEquals(5, actual, "Test failed.");
+    }
+
+    @Test
+    void add2() {
+        System.out.println("Add2 method");
+
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(3,2));   // pass
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(2,3));
+        // fail, expected illegal to be thrown, but nothing was thrown.
+        assertThrows(AccessDeniedException.class, () -> Calculator.add2(3,2));      // fail
     }
 
     @Test
@@ -47,5 +79,13 @@ class CalculatorTest {
     @Test
     void testCase5() {
         System.out.println("Test Case 5");
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1,c2);
+        assertNotSame(c1,c3);
+
     }
 }
