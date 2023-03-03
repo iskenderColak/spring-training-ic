e06096a328adf3a5fbb7afaf837a2c061bf04706
iskenderColak <iclk84@gmail.com>
2023-03-03 21:03:54 +0300

    REST OpenAPI3

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/icolak/Spring19RestOpenApi3Application.java
@@ -0,0 +1,13 @@
+package com.icolak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring19RestOpenApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestOpenApi3Application.class, args);
+    }
+
+}
