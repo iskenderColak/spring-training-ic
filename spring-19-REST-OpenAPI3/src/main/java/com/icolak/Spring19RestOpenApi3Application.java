5e4422fd17bd447bb1fd5ebdca4ba2ff0f0bcb24
iskenderColak <iclk84@gmail.com>
2023-03-04 10:44:47 +0300

    Manipulating the info part

--- spring-19-REST-OpenAPI3/src/main/java/com/icolak/Spring19RestOpenApi3Application.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/icolak/Spring19RestOpenApi3Application.java
@@ -1,9 +1,14 @@
 package com.icolak;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Spring19RestOpenApi3Application {
@@ -15,5 +20,14 @@ public class Spring19RestOpenApi3Application {
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("Icolak Application OpenAPI")
+                .version("v1")
+                .description("Icolak application API documentation" ))
+                .servers(List.of(new Server().url("https://dev.cydeo.com").description("Dev Environment")));
     }
 }
